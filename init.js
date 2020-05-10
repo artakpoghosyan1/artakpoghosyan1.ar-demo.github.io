a830bdee8d2810b376dfffdeabbfe7ac35f923a6
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-10 23:37:30 +0400

    add recording loader, retry and ok buttons

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
 const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const circle = document.querySelector('.circle');
-const play = document.getElementById('play');
+const loader = document.querySelector('.video-loader');
+const retry = document.getElementById('retry');
+const ok = document.getElementById('ok');
 const video = document.getElementById('video');
 const videoWrapper = document.getElementById('video-wrapper');
 
@@ -22,30 +22,33 @@ start.addEventListener('click', function () {
     if (recorder) {
         videoWrapper.classList.remove('show');
 
-        circle.classList.add('animate-circle');
+        loader.classList.add('recording');
         recorder.startRecording(null).then(function (blob) {
             const newBlob = new Blob([blob]);
             const url = URL.createObjectURL(newBlob);
-            localStorage.setItem('video', JSON.stringify(url))
+            video.src = url
         });
+
+        setTimeout(() => {
+            recorder.stopRecording()
+            if(!videoWrapper.classList.contains('show')) {
+                // loader.classList.remove('recording');
+                videoWrapper.classList.add('show');
+            }
+        }, 2000);
     }
 });
 
-stop.addEventListener('click', function () {
-    if (recorder) {
-        circle.classList.remove('animate-circle');
-        recorder.stopRecording();
-        if(!videoWrapper.classList.contains('show')) {
-            videoWrapper.classList.add('show');
-        }
-    }
+retry.addEventListener('click', function () {
+    videoWrapper.classList.remove('show');
+    loader.classList.remove('recording');
 });
 
-play.addEventListener('click', function () {
-    const url = localStorage.getItem('video');
-
-    video.src = JSON.parse(url)
-});
+// play.addEventListener('click', function () {
+//     const url = localStorage.getItem('video');
+//
+//     video.src = JSON.parse(url)
+// });
 
 engine.runRenderLoop(function () {
     if (scene) {
