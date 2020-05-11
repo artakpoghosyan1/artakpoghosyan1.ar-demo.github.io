0a7b61a4c046d2a19f2d90d9bcd3a78eef83afa0
WooFL <wonderfou2@gmail.com>
2020-05-11 19:33:09 +0400

    Set output fps to 30

--- init.js
+++ init.js
@@ -11,7 +11,7 @@ const scene = createScene();
 
 function videoRecorder() {
     if (BABYLON.VideoRecorder.IsSupported(engine)) {
-        return new BABYLON.VideoRecorder(engine);
+        return new BABYLON.VideoRecorder(engine, {fps: 30});
     }
 
     return null
@@ -27,7 +27,7 @@ start.addEventListener('click', function () {
         recorder.startRecording(null).then(function (blob) {
             const newBlob = new Blob([blob]);
             const url = URL.createObjectURL(newBlob);
-            video.src = url
+            video.src = url;
         });
 
         setTimeout(() => {
@@ -36,7 +36,7 @@ start.addEventListener('click', function () {
                 // loader.classList.remove('recording');
                 videoWrapper.classList.add('show');
             }
-        }, 2000);
+        }, 5000);
     }
 });
 
